5ca20f7e43beddf6dc16b00b9bedd943aa984d48
Paul <paul@nanosoft.co.za>
2022-10-28 15:51:37 +0200

    added put, delete and get

--- index.js
+++ index.js
@@ -2,33 +2,56 @@ import express from "express";
 import { join, dirname } from 'path'
 import { Low, JSONFile } from 'lowdb'
 import { fileURLToPath } from 'url'
-import { nanoid } from "nanoid";
+
+import { customAlphabet } from "nanoid";
+const nanoid = customAlphabet('1234567890abcdef', 10)
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, 'db.json')
 const adapter = new JSONFile(file)
 const db = new Low(adapter)
 await db.read()
-db.data ||= { users: [], customers: [], products: [] }
+db.data ||= { users: [], customers: [], products: [], tasks: [] }
 
 const server = express();
 server.use(express.json());
 const PORT = 4000;
 
 server.listen(PORT, () => console.info("Running on http://localhost:" + PORT))
-server.get("/:collection", (req, res) => {
-    const data = db.data[req.params.collection]
+
+server.get("/:collection/:id", (req, res) => {
+    const { collection, id } = req.params
+    const data = db.data[collection]
+
     if (!data) {
-        res.json({ message: req.params.collection + " not a validate collection" })
-    } else {
-        res.send(data)
+        return res.json({ message: collection + " not a validate collection" })
+    }
+
+    const items = data.filter(d => d.id === id)
+    if (items.length) {
+        return res.json(items[0])
+    }
+
+    return res.status(404).json({ message: "Not Found" })
+
+});
+
+
+server.get("/:collection", (req, res) => {
+    try {
+        const { collection } = req.params
+        const data = db.data[collection]
+        return res.status(data ? 200 : 400).json(data || { message: collection + " not a validate collection" })
+    } catch (e) {
+        res.status(500).json({ message: e.message })
     }
 });
 
 server.post("/:collection", async (req, res) => {
     try {
-        const collection = db.data[req.params.collection];
-        collection.push({ id: nanoid(), ...req.body })
+        const { collection } = req.params
+        const data = db.data[collection];
+        data.push({ ...req.body, id: nanoid(5) })
         await db.write()
         res.json({ message: "Created" })
 
@@ -38,14 +61,30 @@ server.post("/:collection", async (req, res) => {
 });
 
 
-server.get("/:collection/:id", (req, res) => {
-    res.send("Testing")
+
+server.delete("/:collection/:id", async (req, res) => {
+    try {
+        const { collection, id } = req.params
+        const data = db.data[collection]
+        const index = data.map(d => d.id).indexOf(id)
+        data.splice(index, 1)
+        await db.write();
+        res.json({ message: "Deleted" })
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
 });
 
-server.delete("/:collection/:id", (req, res) => {
-    res.send("Testing")
-});
-
-server.put("/:collection/:id", (req, res) => {
-    res.send("Testing")
+server.put("/:collection/:id", async (req, res) => {
+    try {
+        const { collection, id } = req.params
+        const dataToUpdate = req.body
+        const data = db.data[collection]
+        const index = data.map(d => d.id).indexOf(id)
+        data[index] = { ...data[index], ...dataToUpdate }
+        await db.write();
+        res.json({ message: "updated" })
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
 });
